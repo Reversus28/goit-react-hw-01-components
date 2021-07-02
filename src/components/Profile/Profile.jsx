0d870b09a754ba575f8a5715c293c099a49e9ffd
Oleksandr Yakovenko <mozg555666@gmail.com>
2021-07-02 13:44:29 +0300

    add code

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+function Profile(props) {
+  console.log('props', props);
+  return (
+    <div class="profile">
+      <div class="description">
+        <img
+          src="https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg"
+          alt="Аватар пользователя"
+          class="avatar"
+        />
+        <p class="name">Petra Marica</p>
+        <p class="tag">@pmarica</p>
+        <p class="location">Salvador, Brasil</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">1000</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">2000</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">3000</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Profile;
+
+// Profile.defaultProps = {
+//   imgUrl:
+//     'https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder',
+// }
+
+// Profile.propTypes = {
+//   imgUrl: Profile.string,
+//   name: Profile.string.isRequired,
+//   price: Profile.number.isRequired,
+// }
